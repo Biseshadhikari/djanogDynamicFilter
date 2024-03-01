306b6106b92c3000355b3ace122a98cf744ae10e
Bisesh Adhikari <adhikaribisesh1590@gmail.com>
2024-03-01 09:26:37 +0545

    Made Filtering Dynamic

--- core/views.py
+++ core/views.py
@@ -6,29 +6,58 @@ from .serializer import ItemSerializer
 
 
 
+# class ItemList(generics.ListAPIView):
+#     serializer_class = ItemSerializer
+
+
+#     def get_queryset(self):
+#         queryset = Item.objects.all()
+
+#         # Filtering based on category
+#         categories_str = self.request.query_params.getlist('category', [])
+#         categories = []
+#         if len(categories_str)>0:
+#             categories = categories_str[0].split(',')
+
+#         if categories:
+#             queryset = queryset.filter(category__in=categories)
+
+#         # Filtering based on destination
+#         destinations_str = self.request.query_params.getlist('destination', [])
+#         destinations = []
+#         if len(destinations_str)>0:
+#             destinations = destinations_str[0].split(',')
+#         if destinations:
+#             queryset = queryset.filter(destination__in=destinations)
+#         print(queryset)
+
+#         return queryset
+
+
+
+
+
+
+
+
+
+
 class ItemList(generics.ListAPIView):
     serializer_class = ItemSerializer
 
-
     def get_queryset(self):
         queryset = Item.objects.all()
+        query_params = self.request.query_params
 
-        # Filtering based on category
-        categories_str = self.request.query_params.getlist('category', [])
-        categories = []
-        if len(categories_str)>0:
-            categories = categories_str[0].split(',')
+        filter_kwargs = {}
 
-        if categories:
-            queryset = queryset.filter(category__in=categories)
+        for param, value in query_params.items():
+            filter_kwargs[f"{param}__in"] = value.split(',')
+        
+        print(filter_kwargs)
 
-        # Filtering based on destination
-        destinations_str = self.request.query_params.getlist('destination', [])
-        destinations = []
-        if len(destinations_str)>0:
-            destinations = destinations_str[0].split(',')
-        if destinations:
-            queryset = queryset.filter(destination__in=destinations)
-        print(queryset)
+        if filter_kwargs:
+            queryset = queryset.filter(**filter_kwargs)
 
         return queryset
+
